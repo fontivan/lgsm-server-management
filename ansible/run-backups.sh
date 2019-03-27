f2b301ce43dbd29f09e623aa01b471b7e0f4774c
Steven Skeard <stevenskeard@gmail.com>
2019-03-26 22:53:37 -0400

    Move scripts to top level directory

--- ansible/run-backups.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source get_ansible_cfg_rc
-ansible-playbook backup-game-servers.yml -i inventory.yml --ask-become-pass
--- ansible/run-ubuntu-maintenence.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source get_ansible_cfg_rc
-ansible-playbook ubuntu-maintenence.yml -i inventory.yml --ask-become-pass
